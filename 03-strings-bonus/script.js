48185ee2a6c194df8c6d0c581e808a4cea9f8b35
giorgiameffe <giorgiameffe03@gmail.com>
2025-02-21 18:30:53 +0100

    feat: exercise 4

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -22,8 +22,12 @@ console.log(city.replace("York", "Delhi")); // Output:  "New Delhi"
 // 4. Template literal
 // Concatena due stringhe usando la sintassi `${var}`
 // e rendi cityNameConcat uguale a 'New York City'
-const cityNameConcat = '';
-console.log(cityNameConcat); // Output: "New York City"
+const cityNameConcat = 'New York City';
+console.log(
+`
+ ${city} ${cityName}
+`
+); // Output: "New York City"
 
 // 5. Tutto in maiscolo
 // Trasforma tutto in maiscuolo la stringa della variabile hello
